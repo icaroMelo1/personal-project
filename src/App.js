92701cfaa874098ef6691eaf9803c3c4dca4f776
Icaro Melo <exemployt@gmail.com>
2021-11-09 10:33:23 -0300

    Component App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 // import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react';
 import './App.css';
 import Provider from './components/Provider';
 import Body from './components/Body';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-function App() {
-  return (
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Provider>
-          <Header />
-          <Body />
-          <Footer />
-        </Provider>
-    // </BrowserRouter>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+        <Provider>
+            <Header />
+            <Body />
+            <Footer />
+          </Provider>
+        {/* </BrowserRouter> */}
+      </div>
+    )
+  }
 }
 
 export default App;
